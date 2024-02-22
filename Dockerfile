bd6a4a080605dc71dace9d1a94a82995669adfc1
SeongJongHo <whdgh9595@gmail.com>
2024-02-22 23:58:51 +0900

    [ADD] docker file 생성, 테스트용 테스트코드 작성

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /app
 
 COPY ./build/libs/cicd-0.0.1-SNAPSHOT.jar app.jar
 
-EXPOSE 8080:8080
+EXPOSE 8080
 
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-jar", "app/app.jar"]
--- src/test/java/com/example/cicd/CicdApplicationTests.java
+++ src/test/java/com/example/cicd/CicdApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.cicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class CicdApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 1);
 	}
 
 }
